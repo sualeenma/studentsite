77c80d96660cf77c9271c46a7e546a0d9286defa
sualeenma <sualee@hawaii.edu>
2023-03-27 00:39:30 -1000

    edited

--- art128/sitename-singlepagesite/js/script.js
+++ art128/sitename-singlepagesite/js/script.js
@@ -15,3 +15,23 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    // TOGGLE NAV 
+    var mymenubutton = document.querySelector('.menu-button');
+    var mytogglenav = document.querySelector('.toggle-nav');
+
+    mymenubutton.addEventListener('click', function () {
+        mytogglenav.classList.toggle('expanded');
+    });
+
+    //STICKY NAV - REMOVE EXPANDED CLASS FOR MOBILE
+    var stickynavlinks = document.querySelectorAll(".sticky nav a");
+    for (var i = 0; i < stickynavlinks.length; i++) {
+        stickynavlinks[i].onclick = function () {
+            mytogglenav.classList.remove("expanded");
+        }
+    };
+
+});
