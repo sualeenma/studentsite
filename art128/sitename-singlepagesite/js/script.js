8e7af83004b2e6e26dbb4391fdecdeee52b8a4c9
sualeenma <sualee@hawaii.edu>
2023-04-05 20:30:21 -1000

    edited

--- art128/sitename-singlepagesite/js/script.js
+++ art128/sitename-singlepagesite/js/script.js
@@ -14,23 +14,12 @@ document.addEventListener('DOMContentLoaded', function() {
 
     });
 
-});
-
-document.addEventListener('DOMContentLoaded', function () {
-
-    // TOGGLE NAV 
-    var mymenubutton = document.querySelector('.menu-button');
-    var mytogglenav = document.querySelector('.toggle-nav');
-
-    mymenubutton.addEventListener('click', function () {
-        mytogglenav.classList.toggle('expanded');
-    });
 
     //STICKY NAV - REMOVE EXPANDED CLASS FOR MOBILE
     var stickynavlinks = document.querySelectorAll(".sticky nav a");
     for (var i = 0; i < stickynavlinks.length; i++) {
         stickynavlinks[i].onclick = function () {
-            mytogglenav.classList.remove("expanded");
+            menunav.setAttribute('data-navstate', 'closed');
         }
     };
 
